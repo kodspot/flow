972eb84dd342a90e85dc0c4febaf0670b8ae2d0c
Kishan Thorat <kishanthorat111@gmail.com>
2026-05-01 04:03:08 +0530

    fix(web): drop R2 incremental cache (no binding) - use defaults

--- apps/web/open-next.config.ts
+++ apps/web/open-next.config.ts
@@ -1,8 +1,4 @@
 // @ts-ignore
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
-// @ts-ignore
-import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache';
 
-export default defineCloudflareConfig({
-  incrementalCache: r2IncrementalCache,
-});
+export default defineCloudflareConfig({});
